e7bbfdea44cfcc35c1585d420f10a6a52c8371cc
Jeryl Khoo <12jerylkhoo@gmail.com>
2026-03-06 04:27:07 +0800

    removed redundant queue

--- backend/rabbitmq/rabbitmq_setup.py
+++ backend/rabbitmq/rabbitmq_setup.py
@@ -24,7 +24,6 @@ bidly_queues = [
     {"name": "Out_Bidded_Notifications", "routing_key": "out.bidded.notifications"},
 
     {"name": "Task_Created_WebSocket", "routing_key": "task.created.websocket"},
-    {"name": "Chat_Connected_WebSocket", "routing_key": "chat.connected.websocket"}, # joshua added: for connect-chat
     {"name": "New_Message_WebSocket", "routing_key": "new.message.websocket"}, # joshua added: for chat-log
 ]
 
