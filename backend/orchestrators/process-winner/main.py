64346cd9cc6eb8d4fe0f2779d51676cfcba8101f
Joshua Lim Quan Xin <joshua.lim.2024@computing.smu.edu.sg>
2026-03-05 17:34:58 +0800

    Revert to 12451d2

--- backend/orchestrators/process-winner/main.py
+++ backend/orchestrators/process-winner/main.py
@@ -34,11 +34,11 @@ def process_winner(ch, method, properties, body):
                 freelancer_id=winner["bidder_id"]
             )
             response = requests.put(f"http://tasks:8005/tasks/{request.task_id}", json=update_task_request.model_dump(mode='json'))
-
+        
         if response.status_code != 200:
             response.raise_for_status()
 
-        # If there is no winner, we can skip the notifications and payment processing
+        # If there is no winner, we can skip the notifications and payment processing 
         # (for now - we can always notify the client about the auction result in the future)
         # or request a new auction to be created if the client wants to try again
         if not winner.get("bidder_id"):
@@ -51,7 +51,6 @@ def process_winner(ch, method, properties, body):
             exchange="bidly",
             routing_key="end.auction.chat",
             body=json.dumps({
-                "task_id": request.task_id, # joshua added this for it create a chat 
                 "client_id": task_data.get("client_id"),
                 "freelancer_id": task_data.get("freelancer_id"),
                 "task_title": task_data.get("title"),
@@ -86,7 +85,7 @@ def process_winner(ch, method, properties, body):
     except Exception as e:
         print(f"Failed to process message: {e}")
         ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
-
+    
 
 channel.basic_qos(prefetch_count=10)
 channel.basic_consume(queue="Process_Winner", on_message_callback=process_winner, auto_ack=False)
--- backend/rabbitmq/rabbitmq_setup.py
+++ backend/rabbitmq/rabbitmq_setup.py
@@ -24,7 +24,6 @@ bidly_queues = [
     {"name": "Out_Bidded_Notifications", "routing_key": "out.bidded.notifications"},
 
     {"name": "Task_Created_WebSocket", "routing_key": "task.created.websocket"},
-    {"name": "Chat_Connected_WebSocket", "routing_key": "chat.connected.websocket"},
 ]
 
 
