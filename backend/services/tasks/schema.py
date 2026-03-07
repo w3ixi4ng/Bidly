a33350e0eedf3359b7920c9a8f6032a1fa317034
Matthew Chan <matcmk@gmail.com>
2026-03-07 23:05:00 +0800

    Update schema.py

--- backend/services/tasks/schema.py
+++ backend/services/tasks/schema.py
@@ -1,40 +1,12 @@
-from pydantic import BaseModel, AwareDatetime
-from typing import Literal, Optional
+from pydantic import BaseModel
+from typing import Dict, Any
 
+class SendEmailRequest(BaseModel):
+    to_email: str
+    template_name: str  
+    dynamic_template_data: Dict[str, Any]
 
-class TaskResponse(BaseModel):
-    task_id: str
-    title: str
-    description: str
-    client_id: str
-    freelancer_id: Optional[str] = None
-    payment_id: str
-    starting_bid: float
-    auction_start_time: AwareDatetime
-    auction_end_time: AwareDatetime
-    auction_status: str
-
-class TaskListResponse(BaseModel):
-    tasks: list[TaskResponse]
-
-
-class TaskCreate(BaseModel):
-    title: str
-    description: str
-    client_id: str
-    freelancer_id: Optional[str] = None
-    payment_id: str
-    starting_bid: float
-    auction_status: Literal["pending", "in-progress", "completed", "cancelled"] = "pending"
-    auction_start_time: AwareDatetime
-    auction_end_time: AwareDatetime
-
-
-class TaskUpdate(BaseModel):
-    title: Optional[str] = None
-    description: Optional[str] = None
-    freelancer_id: Optional[str] = None
-    payment_id: Optional[str] = None
-    auction_status: Optional[Literal["pending", "in-progress", "completed", "cancelled", "no-bids"]] = None
-    auction_start_time: Optional[AwareDatetime] = None
-    auction_end_time: Optional[AwareDatetime] = None
+class SendEmailResponse(BaseModel):
+    status: str
+    message: str
+    status_code: int
