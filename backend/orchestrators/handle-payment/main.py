d445dc31790a806911cf4151821d896a0072dae5
w3ixi4ng <weixiang0124@gmail.com>
2026-03-09 14:06:00 +0800

    to do handle payment

--- backend/orchestrators/handle-payment/main.py
+++ backend/orchestrators/handle-payment/main.py
@@ -20,8 +20,7 @@ async def start_payment_before_create_task(payment_data: StartPaymentData):
             raise HTTPException(status_code=400, detail="User does not have a connected Stripe account")
         
         # then call payment service to connect to stripe and create a payment intent and pass task as metadata
-        # payment svs to not yet setup
-        # payment_intent = service.create_payment_intent(stripe_connected_account_id, payment_data)
+        # payment svc has been created
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -52,6 +51,7 @@ async def payment_success_web_hook(request: Request):
                 "client_id": event["data"]["object"]["metadata"]["client_id"],
             })
             pass
-        # publish to create task
+
+        # publish rabbitmq message to create task 
     
     return {"status": "success"}
